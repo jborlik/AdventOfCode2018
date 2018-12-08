405c1aff70da2a2c4b3a02730cc289ce83317ea8
Jeffrey Borlik <jborlik@gmail.com>
2018-12-07 22:15:50 -0800

    Finshed day 7 both parts.  (Part one had been completed yesterday.)

--- README.md
+++ README.md
@@ -13,8 +13,8 @@ minutes were spent sleeping by each individual guard.
 * Day 5:  Collapsing a (very long) string by matching letters, and then (part 2) collapsing it after removing each letter of the alphabet.  I don't think that I implemented it in as time-efficient way as possible, as each "collapsing" took a couple of minutes.  But, brute force worked for a non-competitive entry.  I had started out by trying to implement something where the string/list length wouldn't be altered (i.e. replace chars with a sentinel) but abandoned it when I started converting the string to a list first.  (I wrongly thought list item removal was O(1).)
 * Day 6:  On a grid, found the area of influence for a particular point (for Part 1), and the area with a total distance to all points less than a critical value.  This one
 involved iterating through numpy arrays.
-* Day 7:
-
+* Day 7:  Had to figure out a graph of process, and then the order to do the steps.  Used a class for a node (step of the process) with multiple parents and multiple children.  Part two had another structure for workers, and a simulation of parallel
+work.  Took a bit longer than others.
 * Day 8:  Used a little class to model a directed graph, parsed recursively and inspected recursively.
 
 
---
+++ day7.py
@@ -0,0 +1,134 @@
+#import re
+#import numpy as np
+
+with open('day7.dat') as datafile:
+    alldata = [ (x[5],x[36]) for x in datafile.readlines()]
+
+steptimebase = 60
+
+
+# test:   CABDFE
+testdata = [ ('C','A'), ('C','F'),('A','B'),('A','D'),('B','E'),('D','E'),('F','E')]
+workers = [ [0,0], [0,0] ] # for test
+
+#steptimebase = 0
+#alldata = testdata
+
+class Worker:
+    def __init__(self):
+        self.currentNode = None
+        self.remainingTime = 0
+    def __repr__(self):
+        if self.currentNode == None:
+            return "[No work]"
+        else:
+            return "[Working. Node={} remaining time={}]".format(self.currentNode.name,self.remainingTime)
+
+workers = [ Worker(), Worker(), Worker(), Worker(), Worker() ]
+#workers = [ Worker(), Worker() ]
+
+class Node:
+    def __init__(self,sname):
+        self.name = sname
+        self.parents = {}
+        self.children = {}
+        self.value = ord(sname[0]) - ord('A') + 1
+    def addParent(self,parentNode):
+        self.parents[parentNode.name] = parentNode
+        parentNode.children[self.name] = self
+    def __str__(self):
+        return "{}: parents={} children={}\n".format(self.name, self.parents, self.children)        
+    def __repr__(self):
+        return "{}".format(self.name)        
+
+nodes = {}
+def getOrCreateNode(sName,nodes):
+    if sName in nodes:
+        return nodes[sName]
+    newnode = Node(sName)
+    nodes[sName] = newnode
+    return newnode
+
+
+for aIns in alldata:
+    sourceNode = getOrCreateNode(aIns[0],nodes)
+    destNode = getOrCreateNode(aIns[1],nodes)
+    destNode.addParent(sourceNode)
+
+# we now have the connected network of steps
+
+def solvePart1():
+    startNodes = []
+    for sName,aNode in nodes.items():
+        if len(aNode.parents) == 0:
+            startNodes.append(aNode)
+
+    order = []
+    while len(startNodes) > 0:
+        startNodes = sorted(startNodes, key=lambda x: x.name)
+        print(startNodes)
+        aNode = startNodes.pop(0)
+        order.append(aNode)
+        for sn, childNode in aNode.children.items():
+            del childNode.parents[aNode.name]
+            if len(childNode.parents) == 0:
+                startNodes.append(childNode)
+
+    orderstringlist = []
+    for aNode in order:
+        orderstringlist.append(aNode.name)
+
+    orderstring = "".join(orderstringlist)
+
+    print("orderstring=",orderstring)
+
+def workersAreComplete(workerlist):
+    for aworker in workerlist:
+        if aworker.currentNode != None:
+            return False
+    return True
+
+def loadWorkers(workerlist, availablenodes):
+    for aWorker in workerlist:
+        if aWorker.currentNode == None:  # no current assignment
+            if len(availablenodes) > 0:
+                aNode = availablenodes.pop(0)
+                aWorker.currentNode = aNode
+                aWorker.remainingTime = steptimebase + aNode.value # remaining time
+
+def incrementTime(workerlist, availablenodes):
+    for aWorker in workerlist:
+        if aWorker.currentNode != None:
+            aWorker.remainingTime -= 1
+            if aWorker.remainingTime == 0:
+                for sn, childNode in aWorker.currentNode.children.items():
+                    del childNode.parents[aWorker.currentNode.name]
+                    if len(childNode.parents) == 0:
+                        availablenodes.append(childNode)
+                aWorker.currentNode = None
+                
+def solvePart2():
+    startNodes = []
+
+    for _,aNode in nodes.items():
+        if len(aNode.parents) == 0:
+            startNodes.append(aNode)
+
+    loadWorkers(workers,startNodes)
+
+    # we now have available nodes and workers
+    # start incrementing the clock
+    time = 0
+    
+    while (not workersAreComplete(workers)) or len(startNodes) > 0:
+        print("Time {}: {}".format(time,workers))
+        incrementTime(workers,startNodes)
+        time += 1
+        startNodes = sorted(startNodes, key=lambda x: x.name)
+        loadWorkers(workers,startNodes)
+
+    print("Time to completion: ", time)
+
+#solvePart1()
+solvePart2()
+
